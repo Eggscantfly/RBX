56dade64772e36531bad803a2cd4b93df47f6ef7
Eggscantfly <103383950+Eggscantfly@users.noreply.github.com>
2026-02-14 00:51:56 -0600

    Rename CPlayer to CActor and update structure

--- Include/Game/CActor/CActor.h
+++ Include/Game/CActor/CActor.h
@@ -1,5 +1,5 @@
-#ifndef CPLAYER_H
-#define CPLAYER_H
+#ifndef CACTOR_H
+#define CACTOR_H
 
 typedef struct {
     char* typeInfo;
@@ -9,11 +9,13 @@ typedef struct {
 
 extern char CVector2_TypeInfo[];
 
-typedef struct CPlayer {
+typedef struct CActor {
     char unk_0x0[0x8];
     float posX;              // 0x8
     float posY;              // 0xC
-    char unk_0x10[0x1D8];
+    char unk_0x10[0x1C8];
+    int entityType;          // 0x1D8
+    char unk_0x1DC[0xC];
     int facingDirection;     // 0x1E8 (0 = right, 1 = left)
     char unk_0x1EC[0xC];
     float targetX;           // 0x1F8
@@ -22,11 +24,12 @@ typedef struct CPlayer {
     int health;              // 0x228
     char unk_0x22C[0x14];
     int state;               // 0x240
-} CPlayer;
+} CActor;
 
-extern CPlayer** g_ppPlayer;  // 0x801FACE0
+struct CPlayer;
+extern struct CPlayer** g_ppPlayer;  // 0x801FACE0
 
-void healthSub(void* player, void* damageSource);
-void flipCharacter(int param_1);
+void healthSub(CActor* player, CActor* damageSource);
+void flipCharacter(CActor* actor);
 
 #endif
